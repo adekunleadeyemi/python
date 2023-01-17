7e0bab1cacfbb87652f280ab90b6eecb23ffa27b
Adekunle <adeyemi.adekunle02@gmail.com>
2023-01-17 03:10:00 +0000

    Learning python code

---
+++ strings.py
@@ -0,0 +1,10 @@
+print("Today is a good day to learn python")
+print('python is fun')
+print("hello" + " world")
+print('We can include "quotes" in strings')
+
+GREETINGS = "Hello"
+
+name = input("Please enter your name:")
+
+print(GREETINGS + '' + name)
